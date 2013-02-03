d9ddef225a991275ad9411ed64bee690671eaf28
Mike <mcueno2@uic.edu>
2013-02-02 22:32:51 -0600

    Starting pipelining

--- src/statsh.cpp
+++ src/statsh.cpp
@@ -11,50 +11,96 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <vector>
 using namespace std;
+
+/* Struct for holding relevant information pretaining to the history of the command run 
+ * within the shell for stats reporting when user enters "stats" as a command 
+ */
+struct history{
+  struct rusage usage_stats;
+  char* proc_name;
+};
+
+#define READ 0
+#define WRITE 1
 
 int main(int argc, char** argv){
 
-  string input; 
-  pid_t child;
+  string input;                       // Input string for determining how large a char array we will need
+  pid_t child;                        // Pid of child process
+  vector<history*> history_list;      // Vector of old commands run along with their rusage structs
+  int pipefd[2];
 
   cout << ("statsh ~> ");
   getline(cin, input);    // input now holds the line form stdin cin
   
   while(input.compare("exit")!=0){
-    char* cstyle = new char[input.length()+1];  
-    char* tokens[(input.length()+1)/2];
-    strcpy(cstyle, input.c_str());    // put input into a char array
 
-    int i =0;
+    /* Check for stats command from user and show appropriate stats if appropriate */
+    if(input.compare("stats")==0){
+      cout << "Process Name: User time | System time" << endl;
+      for(int i = 0; i<history_list.size();i++){
+        cout << history_list[i]->proc_name << ": " ;
+        cout << history_list[i]->usage_stats.ru_utime.tv_sec + (history_list[i]->usage_stats.ru_utime.tv_usec / 1000000.0);
+        cout << " | ";
+        cout << history_list[i]->usage_stats.ru_utime.tv_sec + (history_list[i]->usage_stats.ru_utime.tv_usec / 1000000.0);
+        cout << endl;
+      }
+    }else{
 
-    tokens[i] = strtok(cstyle, " ");
+      char* cstyle = new char[input.length()+1];  
+      char* tokens[(input.length()+1)/2];
+      strcpy(cstyle, input.c_str());    // put input into a char array
 
-    while(tokens[i]){
-      i++;
-      tokens[i] = strtok(NULL, " ");
-    }
+      /* Create the argument list of null terminated char arrays */
+      int i =0;
+      tokens[i] = strtok(cstyle, " ");
+      while(tokens[i]){
+        i++;
+        tokens[i] = strtok(NULL, " ");
+      }
+      /* Now tokens is a char array of null terminated tokens */
 
-    // Now tokens is a char array of null terminated tokens 
-  
-    if((child = fork())<0){
-      cout << "Could not create child process" << endl;
-      exit(0);
-    }
-
-    if(child==0){  // Inside Child code 
-    
-      if(execvp(tokens[0], tokens) <= 0){
-        cout << "Error in Exec" << endl;
+      /* create the pipe */
+      if (pipe2(pipefd, 0) < 0){
+        cout << "pipe failure" << endl;
         exit(0);
       }
-    
-    }else {
-      waitpid(child, 0, 0);
-      cout << ("statsh ~> ");
-      getline(cin, input);    // input now holds the line form stdin cin
+
+      /* Fork the child to run the command in tockens[0] with arglist tokens */
+      if((child = fork())<0){
+        cout << "Could not create child process" << endl;
+        exit(0);
+      }
+
+      if(child==0){  // Inside Child process 
+        close(pipefd[1]);    /* Close unsued write end */
+      
+        if(execvp(tokens[0], tokens) <= 0){
+          cout << "Error in Exec" << endl;
+          exit(0);
+        }
+      
+      }else {   // Inside parent process
+        int *status = new int;
+        struct rusage usage;
+        wait4(child, status, 0, &usage);
+
+        history *record = new history;
+        record->usage_stats = usage;
+        record->proc_name = tokens[0];
+        history_list.push_back(record);
+      }
     }
+    /* Program loop */
+    cout << ("statsh ~> ");
+    getline(cin, input);    // input now holds the line form stdin cin
   }
 
-    return 0;
-  }
+  return 0;
+} /* End of main */
+
