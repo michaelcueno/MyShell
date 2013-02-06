547f65e70ade9afd8818d0f1f0ed77696feca3e1
Mike <mcueno2@uic.edu>
2013-02-06 17:29:36 -0600

    testing

--- src/statsh.cpp
+++ src/statsh.cpp
@@ -28,15 +28,15 @@ void printStats(vector<history*>);
 char** tokenize(string input, char** tokens, const char* delim);         // Returns an array of null terminated null pointers
 void printChar(char** x);
 
-#define READ 0
-#define WRITE 1
+#define IN 0   // read end
+#define OUT 1  // write end
 
 int main(int argc, char** argv){
 
   string input;                       // Input string for determining how large a char array we will need
   pid_t child;                        // Pid of child process
   vector<history*> history_list;      // Vector of old commands run along with their rusage structs
-  int pipefd[2];                      // File descriptors 
+  int pipefd[2];                      // File descriptors (pipefd[0] = Read end, pipefd[1] = write end)
   int i = 0;                          // index variable for loops
   int cmd_index = 0;              // index for the commands array
 
@@ -50,18 +50,26 @@ int main(int argc, char** argv){
       printStats(history_list);
     }else{
 
-      /* create the pipe */
-      if (pipe2(pipefd, 0) < 0){
-        cout << "pipe failure" << endl;
-        exit(0);
-      }
-
+      int stdout = dup2(1);   // save a copy of stdout for last child 
       /* Create the commands list */
       char* commands[input.length()];
       tokenize(input, commands, "|");
 
       while(commands[cmd_index]) { // For each command 
 
+        /* create the pipe */
+        if (pipe2(pipefd, 0) < 0){
+          cout << "pipe failure" << endl;
+          exit(0);
+        }
+        /* So now I have 4 file desciptors, stdin => stdout and pipefd[0] => pipefd[1]
+         * The parent needs to be able to send bytes to the child via this descriptor, 
+         * thus, the parent replaces stdout with pipefd[OUT] while the child holds pipefd[IN]
+         * The child then writes to stdout, this is not good.. We need the child to write to 
+         * a new? file descriptor called next possibly and the next child will hold the next[IN]
+         */
+
+        /* Create the specific job char array */
         char* tokens[strlen(commands[cmd_index])];
         tokenize(commands[cmd_index], tokens, " ");
       
@@ -69,26 +77,34 @@ int main(int argc, char** argv){
         /* Fork the child to run the command in tockens[0] with arglist tokens */
         if((child = fork())<0){
           cout << "Could not create child process" << endl;
-          exit(0);
+          exit(0) ;
         }
 
         if(child==0){  // Inside Child process 
+          close(pipefd[OUT]); // close write end (child will always write to stdout)
+          dup2(pipefd[IN], IN);  // set stdin to read end of pipe
+          close(pipefd[IN]);
         
           if(execvp(tokens[0], tokens) <= 0){
             cout << "Error in Exec" << endl;
             exit(0);
           }
-        
-        }else {   // Inside parent process
-          int *status = new int;
-          struct rusage usage;
-          wait4(child, status, 0, &usage);
+        } 
+        /* In the parent */
+        close(pipefd[IN]); // close write end (Should take input from stdin (since child writes to stdout))
+        dup2(pipefd[OUT],OUT); // set stdout to write end of file descriptor 
+        close(pipefd[OUT]);
 
-          history *record = new history;
-          record->usage_stats = usage;
-          record->proc_name = tokens[0];
-          history_list.push_back(record);
-        }
+
+        int *status = new int;
+        struct rusage usage;
+        wait4(child, status, 0, &usage);
+        
+        history *record = new history;
+        record->usage_stats = usage;
+        record->proc_name = tokens[0];
+        history_list.push_back(record);
+
       cmd_index++;
       }
 
